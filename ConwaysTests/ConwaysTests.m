9d1b9d556c9badbe921848c8756ea1e1da464f7b
Dominic Wroblewski <domness@gmail.com>
2011-05-29 00:55:09 +0100

    Included test for previous commit.

--- ConwaysTests/ConwaysTests.m
+++ ConwaysTests/ConwaysTests.m
@@ -45,4 +45,22 @@
   STAssertEquals(one, cell2.neighbours_count, @"Cell2 had incorrect neighbours");
 }
 
+- (void)testShouldOnlyHaveASpecificCellOnce
+{
+  Cell * cell1 = [[Cell alloc] init];
+  Cell * cell2 = [[Cell alloc] init];
+
+  NSUInteger zero = 0;
+  NSUInteger one = 1;
+
+  STAssertEquals(zero, cell1.neighbours_count, @"Cell1 had incorrect neighbours");
+  STAssertEquals(zero, cell2.neighbours_count, @"Cell2 had incorrect neighbours");
+
+  [cell1 addNeighbour:cell2];
+  [cell1 addNeighbour:cell2];
+
+  STAssertEquals(one, cell1.neighbours_count, @"Cell1 had incorrect neighbours");
+  STAssertEquals(one, cell2.neighbours_count, @"Cell2 had incorrect neighbours");
+}
+
 @end
